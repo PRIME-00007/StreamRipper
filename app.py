c51982096754fd570e33129425d4598a7ac6ba04
PRIME <eloghosaomoruyi007@gmail.com>
2025-10-25 16:08:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def download_mp3():
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT via env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
